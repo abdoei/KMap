96cae43e8866553cbf369595a3712c8d17fb5a94
[Abdo Eid] <[abdoeid.eg@ieee.org]>
2023-07-16 11:41:28 +0300

    code refactoring and annotating and changed the readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# KMap
-Karnaugh Map solver
+# KMap solver
+Karnaugh Map solver on 4 var map: this is a side project we made in the sophomore year while taking a Digital Circuit Design course to help solving Karnaugh Maps. It's not perfect, but it works. It's a simple program that takes a Karnaugh Map expression and gives you the solution. It can handle up to 4 variables.
+
+## How to use
+It is a simple kMap.exe file, just run it and follow the instructions. 
+
+## Example
+when you run the program, you will see the following:
+```
+Your literals are A, B, C and D capital or small letters (you can mix in between) in addition to these letters you only can enter the plus sign "+" and the apostrophe sign to negate the literal (e.g., A' or b')
+
+For example A'B'CD+A'BCD+ABCD+AB'CD+ABC'D'+ABC'D+ ABCD' will give you the following kMap:
+
+0, 0, 1, 0,
+0, 0, 1, 0,
+1, 1, 1, 1,
+0, 0, 1, 0,
+```
+you can enter the expression you want to solve, and the program will give you the solution.
+```
+A'Bc'+abc'+ ab'c +a'b'c
+```
+will give you the following:
+```
+your input is: 
+A'BC'+ABC'+AB'C+A'B'C
+
+0, 0, 1, 1, 
+1, 1, 0, 0,
+1, 1, 0, 0, 
+0, 0, 1, 1, 
+```
--- kMap.cpp
+++ kMap.cpp
@@ -4,99 +4,50 @@
 
 using namespace std;
 
+// function prototypes
+void enterance_message();
+void prepair_input(char arr[], char const dummy_arr[]);
+int divide_terms(char const arr[], char terms[][8], int numOfLiterals[], int* lastTermIndex);
+void convertToBinary(int termsBinaryValue[][4], char terms[][8], int lastTermIndex);
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    // cout.tie(NULL);
-    // cin.tie(NULL);
-    cout << "Your literals are A, B, C and D capital or small letters (you can mix in between) in addition to these letters you only can enter the plus sign \"+\" and the apostrophe sign to negate the literal (e.g., A' or b') \n";
     int flag = 0; // General Purpose flag
-    char arr[maxInputLetters] = "", terms[maxInputTerms][8] = {};
-    cin >> arr;
+    enterance_message();
 
-    // divide into terms
+    char dummy_arr[maxInputLetters] = "A'B'CD+A'BCD+ABCD+AB'CD+AB C'D'+ABC'D+ ABCD' ", terms[maxInputTerms][8] = {};
+    // we use getline to get the input with spaces or not
+    cin.getline(dummy_arr, maxInputLetters);
+    
+    // removing spaces
+    char arr[maxInputLetters] = "";
+
+    prepair_input(arr, dummy_arr);
+    
+    // in case of truncation
+    cout << "your input is: " << endl;
+    cout << arr << endl;
+
     int lastTermIndex = 0;
-    for (int i = 0; arr[i] != '\0'; i++, lastTermIndex++)
-    {
-        for (int j = 0; !(arr[i] == '+' || arr[i] == '\0'); j++)
-        {
-            terms[lastTermIndex][j] = arr[i];
-            if (!(arr[i] == '+' || arr[i] == '\'' || arr[i] == 'a' || arr[i] == 'b' || arr[i] == 'c' || arr[i] == 'd' || arr[i] == 'A' || arr[i] == 'B' || arr[i] == 'C' || arr[i] == 'D'))
-            {
-                cout << "Wrong input" << endl;
-                return 1;
-            }
-            i++; // bypass the + sign
-        }
-    }
-
-    // calc the num of the lit of each term
     int numOfLiterals[maxInputTerms] = {0};
-    for (int i = 0; i < lastTermIndex; i++)
-    {
-        flag = 0;
-        for (int j = 0; terms[i][j] != '\0'; j++)
-        {
-            if (terms[i][j] != '\'')
-                flag++;
-        }
-        numOfLiterals[i] = flag;
-    }
-
+    if(divide_terms(arr, terms, numOfLiterals, &lastTermIndex))
+        return 1;
+    
     // calc the binary of each term
-    // initiate the termsBinaryValue array with -1 to be like {{-1, -1, -1, -1},...}
     int termsBinaryValue[maxInputTerms][4];
-    for (int i = 0; i < maxInputTerms; i++)
-    {
-        for (int j = 0; j < 4; j++)
-            termsBinaryValue[i][j] = -1;
-    }
+    convertToBinary(termsBinaryValue, terms, lastTermIndex);
 
-    // convert to binary
-    for (int i = 0; i < lastTermIndex + 1; i++)
-    {
-        for (int j = 0; j < 2 * numOfLiterals[i]; j++) // maximum iteration is applied when all lit are negated
-        {
-            if ((terms[i][j] == 'a' || terms[i][j] == 'A') && terms[i][j + 1] != '\'')
-                termsBinaryValue[i][0] = 1;
-            else if ((terms[i][j] == 'a' || terms[i][j] == 'A') && terms[i][j + 1] == '\'')
-            {
-                termsBinaryValue[i][0] = 0;
-                j++;
-            }
-            else if ((terms[i][j] == 'b' || terms[i][j] == 'B') && terms[i][j + 1] != '\'')
-                termsBinaryValue[i][1] = 1;
-            else if ((terms[i][j] == 'b' || terms[i][j] == 'B') && terms[i][j + 1] == '\'')
-            {
-                termsBinaryValue[i][1] = 0;
-                j++;
-            }
-            else if ((terms[i][j] == 'c' || terms[i][j] == 'C') && terms[i][j + 1] != '\'')
-                termsBinaryValue[i][2] = 1;
-            else if ((terms[i][j] == 'c' || terms[i][j] == 'C') && terms[i][j + 1] == '\'')
-            {
-                termsBinaryValue[i][2] = 0;
-                j++;
-            }
-            else if ((terms[i][j] == 'd' || terms[i][j] == 'D') && terms[i][j + 1] != '\'')
-                termsBinaryValue[i][3] = 1;
-            else if ((terms[i][j] == 'd' || terms[i][j] == 'D') && terms[i][j + 1] == '\'')
-            {
-                termsBinaryValue[i][3] = 0;
-                j++;
-            }
-        }
-    }
+
 
     // Generate values from terms
-    int permutations[8][3] = {{0, 0, 0},
-                              {1, 0, 0},
-                              {0, 1, 0},
-                              {1, 1, 0},
-                              {0, 0, 1},
-                              {1, 0, 1},
-                              {0, 1, 1},
-                              {1, 1, 1}};
+    bool permutations[8][3] =  {{0, 0, 0},
+                                {1, 0, 0},
+                                {0, 1, 0},
+                                {1, 1, 0},
+                                {0, 0, 1},
+                                {1, 0, 1},
+                                {0, 1, 1},
+                                {1, 1, 1}};
     int finalValues[16] = {};
 
     // looping through the entered terms to ...
@@ -135,11 +86,13 @@ int main()
     }
 
     // rearrange the terms in the kMap
-    int map[4][4] = {{0, 1, 3, 2},
-                     {4, 5, 7, 6},
-                     {12, 13, 15, 14},
-                     {8, 9, 11, 10}};
-    int kMap[4][4] = {0};
+    short map[4][4] = { {0, 1, 3, 2},
+                        {4, 5, 7, 6},
+                        {12, 13, 15, 14},
+                        {8, 9, 11, 10}};
+
+    
+    bool kMap[4][4] = {0};
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -155,7 +108,116 @@ int main()
         {
             cout << kMap[i][j] << ", ";
         }
+        // backspace to remove the last comma
+        cout << "\b\b\b";
         cout << endl;
     }
     return 0;
 }
+
+// function to print the enterance message
+void enterance_message(){
+        cout << "Your literals are A, B, C and D capital or small letters (you can mix in between) in addition to these letters you only can enter the plus sign \"+\" and the apostrophe sign to negate the literal (e.g., A' or b') \n";
+    cout << "For example A'B'CD+A'BCD+ABCD+AB'CD+ABC'D'+ABC'D+ABCD' will give you the following kMap: \n\n";
+    cout << "0, 0, 1, 0, \n";
+    cout << "0, 0, 1, 0, \n";
+    cout << "1, 1, 1, 1, \n";
+    cout << "0, 0, 1, 0, \n";
+}
+// remove the spaces and capitalize the input
+void prepair_input(char arr[], char const dummy_arr[]){
+    for (int i = 0, j = 0; dummy_arr[i] != '\0'; i++)
+    {
+        if (dummy_arr[i] != ' ')
+        {
+            arr[j] = dummy_arr[i];
+            j++;
+        }
+    }
+
+    // capitalize the input
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        if (arr[i] >= 'a' && arr[i] <= 'd')
+            arr[i] -= 32;
+    }
+}
+
+// function to divide the input into terms and calculate the number of literals in each term
+int divide_terms(char const arr[], char terms[][8], int numOfLiterals[], int* lastTermIndex){
+    
+    for (int i = 0; arr[i] != '\0'; i++, (*lastTermIndex)++)
+    {
+        for (int j = 0; !(arr[i] == '+' || arr[i] == '\0'); j++)
+        {
+            terms[*lastTermIndex][j] = arr[i];
+            if (!(arr[i] == '+' || arr[i] == '\'' || arr[i] == 'A' || arr[i] == 'B' || arr[i] == 'C' || arr[i] == 'D'))
+            {
+                cout << "Wrong input" << endl;
+                return 1;
+            }
+            i++; // bypass the + sign
+        }
+    }
+
+    int flag;
+    // calc the num of the literals in each term
+    for (int i = 0; i < *lastTermIndex; i++)
+    {
+        flag = 0;
+        for (int j = 0; terms[i][j] != '\0'; j++)
+        {
+            if (terms[i][j] != '\'')
+                flag++;
+        }
+        numOfLiterals[i] = flag;
+    }
+    return 0;
+}
+
+// function to convert the terms to binary
+void convertToBinary(int termsBinaryValue[][4], char terms[][8], int lastTermIndex){
+
+    // initiate the termsBinaryValue array with -1 to be like {{-1, -1, -1, -1},...}
+    for (int i = 0; i < maxInputTerms; i++)
+    {
+        for (int j = 0; j < 4; j++) termsBinaryValue[i][j] = -1;
+    }
+
+    // convert to binary
+    for (int i = 0; i < lastTermIndex + 1; i++)
+    {
+        for (int j = 0; j < 2 * 4; j++) // maximum iteration is applied when all lit are negated
+        {
+                 if ((terms[i][j] == 'A') && terms[i][j + 1] != '\'')
+                termsBinaryValue[i][0] = 1;
+            else if ((terms[i][j] == 'A') && terms[i][j + 1] == '\'')
+            {
+                termsBinaryValue[i][0] = 0;
+                j++;
+            }
+            else if ((terms[i][j] == 'B') && terms[i][j + 1] != '\'')
+                termsBinaryValue[i][1] = 1;
+            else if ((terms[i][j] == 'B') && terms[i][j + 1] == '\'')
+            {
+                termsBinaryValue[i][1] = 0;
+                j++;
+            }
+            else if ((terms[i][j] == 'C') && terms[i][j + 1] != '\'')
+                termsBinaryValue[i][2] = 1;
+            else if ((terms[i][j] == 'C') && terms[i][j + 1] == '\'')
+            {
+                termsBinaryValue[i][2] = 0;
+                j++;
+            }
+            else if ((terms[i][j] == 'D') && terms[i][j + 1] != '\'')
+                termsBinaryValue[i][3] = 1;
+            else if ((terms[i][j] == 'D') && terms[i][j + 1] == '\'')
+            {
+                termsBinaryValue[i][3] = 0;
+                j++;
+            }
+        }
+    }
+}
+
